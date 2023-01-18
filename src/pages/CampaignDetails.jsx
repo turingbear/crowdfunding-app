7c126d41c0c31d923f874c75df63af0413067f55
nsect <nsecttheory@gmail.com>
2023-01-18 15:20:52 -0800

    image fix 3

--- src/pages/CampaignDetails.jsx
+++ src/pages/CampaignDetails.jsx
@@ -79,7 +79,7 @@ const CampaignDetails = () => {
                 <img
                   src={thirdweb}
                   alt="user"
-                  className="w-[60px] h-[60px] object-contain"
+                  className="w-1/2 h-1/2 object-contain"
                 />
               </div>
               <div>
