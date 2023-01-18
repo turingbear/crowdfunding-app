dad25039e245c973d3d545cdad1f3f57b3c4d380
nsect <nsecttheory@gmail.com>
2023-01-18 15:36:01 -0800

    animate change

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ const Loader = () => {
   return (
     <div className="fixed inset-0 z-10 h-screen bg-[rgba(0,0,0,0.7)] flex flex-col items-center justify-center">
       
-      <p className="mt-[20px] font-epilogue font-bold text-[20px] text-center text-white animate-ping">
+      <p className="mt-[20px] font-epilogue font-bold text-[20px] text-center text-white animate-pulse">
         Transaction is in progress <br /> Please wait...
       </p>
     </div>
